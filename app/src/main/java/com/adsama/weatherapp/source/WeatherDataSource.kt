f77508f735a6acbbb45cbc5053e7ba38d84ee098
Paran Soral <paransharma94@gmail.com>
2023-08-07 14:43:47 +0530

    Data source interface callbacks added, return types removed

--- app/src/main/java/com/adsama/weatherapp/source/WeatherDataSource.kt
+++ app/src/main/java/com/adsama/weatherapp/source/WeatherDataSource.kt
@@ -6,14 +6,29 @@ import com.adsama.model.SearchResponse
 
 interface WeatherDataSource {
 
-    suspend fun getForecast(location: String) : ForecastResponse
+    suspend fun getForecast(location: String, loadForecastCallback: LoadForecastCallback)
 
-    suspend fun getSearchResult(location: String) : List<SearchResponse>
+    suspend fun getSearchResult(location: String, loadSearchCallback: LoadSearchCallback)
 
-    suspend fun getAllSavedLocations(): List<PersistedWeatherModel>
+    suspend fun getAllSavedLocations(loadSavedCallback: LoadSavedCallback)
 
-    suspend fun saveLocation(persistedWeatherModel: PersistedWeatherModel) : String
+    suspend fun saveLocation(persistedWeatherModel: PersistedWeatherModel)
 
-    suspend fun deleteLocation(persistedWeatherModel: PersistedWeatherModel): String
+    suspend fun deleteLocation(persistedWeatherModel: PersistedWeatherModel)
+
+    interface LoadForecastCallback {
+        fun onForecastLoaded(forecastResponse: ForecastResponse)
+        fun onError(t: Throwable)
+    }
+
+    interface LoadSearchCallback {
+        fun onSearchLoaded(searchResponse: List<SearchResponse>)
+        fun onError(t: Throwable)
+    }
+
+    interface LoadSavedCallback {
+        fun onSavedLoaded(savedList: List<PersistedWeatherModel>)
+        fun onError(t: Throwable)
+    }
 
 }
